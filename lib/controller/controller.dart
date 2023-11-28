b61eed0c199697ac4fbdd49f0255f783682f668a
Murat Iyigun <miyigun@hotmail.com>
2023-11-28 19:54:03 +0300

    Commit 2

---
+++ lib/controller/controller.dart
@@ -0,0 +1,11 @@
+import 'package:pdf_example/controller/data.dart';
+
+void loadInitialData() {
+  tableElements.add(titles);
+  tableElements.add(tableRow1);
+  tableElements.add(tableRow2);
+  tableElements.add(tableRow3);
+  tableElements.add(tableRow4);
+  tableElements.add(tableRow5);
+  tableElements.add(tableRow6);
+}
---
+++ lib/controller/data.dart
@@ -0,0 +1,63 @@
+List<List<String>> tableElements = [];
+
+List<String> titles = [
+  "table title column1",
+  "table title column2",
+  "table title column3",
+  "table title column4"
+];
+List<String> tableRow1 = [
+  "table row1 column1",
+  "table row1 column2",
+  "table row1 column3",
+  "table row1 column4"
+];
+
+List<String> tableRow2 = [
+  "table row2 column1",
+  "table row2 column2",
+  "table row2 column3",
+  "table row2 column4"
+];
+
+List<String> tableRow3 = [
+  "table row3 column1",
+  "table row3 column2",
+  "table row3 column3",
+  "table row3 column4"
+];
+
+List<String> tableRow4 = [
+  "table row4 column1",
+  "table row4 column2",
+  "table row4 column3",
+  "table row4 column4"
+];
+
+List<String> tableRow5 = [
+  "table row5 column1",
+  "table row5 column2",
+  "table row5 column3",
+  "table row5 column4"
+];
+
+List<String> tableRow6 = [
+  "table row6 column1",
+  "table row6 column2",
+  "table row6 column3",
+  "table row6 column4"
+];
+
+List<String> listElements = [
+  "list material1",
+  "list material2",
+  "list material3",
+  "list material4",
+  "list material5",
+  "list material6",
+];
+
+String paragraph =
+    "The flame does not illuminate itself as much as it illuminates other things. The wise are also like this. Friedrich Nietzsche";
+
+
